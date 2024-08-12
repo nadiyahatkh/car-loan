ad3feac0be46302de5f9ab4f4cf2094c4cfaab14
nadiyahatkh <nadiyahatikah843@gmail.com>
2024-08-12 17:50:00 +0700

    new

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,120 @@
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import Navbar from "./navbar";
+import { Table, TableBody, TableCaption, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
 
 
 
 export default function Home() {
+  const invoices = [
+    {
+      invoice: "INV001",
+      paymentStatus: "Paid",
+      paymentMethod: "Credit Card",
+    },
+    {
+      invoice: "INV002",
+      paymentStatus: "Pending",
+      paymentMethod: "PayPal",
+    },
+    {
+      invoice: "INV003",
+      paymentStatus: "Unpaid",
+      paymentMethod: "Bank Transfer",
+    },
+    {
+      invoice: "INV004",
+      paymentStatus: "Paid",
+      paymentMethod: "Credit Card",
+    },
+    {
+      invoice: "INV005",
+      paymentStatus: "Paid",
+      paymentMethod: "PayPal",
+    },
+    {
+      invoice: "INV006",
+      paymentStatus: "Pending",
+      paymentMethod: "Bank Transfer",
+    },
+    {
+      invoice: "INV007",
+      paymentStatus: "Unpaid",
+      paymentMethod: "Credit Card",
+    },
+  ]
   return (
     <>
-
       <div className="top-section">
-        {/* Konten atau komponen navbar atau header bisa dimasukkan di sini */}
+        <Navbar />
+
+        <div className="max-w-screen-xl mx-auto px-4 py-2 z-[100]">
+        <div className="grid gap-4 grid-cols-2">
+          <Card className="rounded-none p-2">
+            <CardContent>
+              <div className="flex justify-between">
+                <p>Available Now</p>
+                <img src="veloc.png" alt="veloc" className="" />
+
+              </div>
+            </CardContent>
+          </Card>
+          <Card className="rounded-none">
+            <CardContent>
+            <div className="flex justify-between">
+                <p>Available Now</p>
+                <img src="veloc.png" alt="veloc" className="" />
+
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+
+        </div>
       </div>
-      <div className="bottom-section">
-        {/* <Component {...pageProps} /> */}
+      <div className="bottom-section p-6">
+        <div className="max-w-screen-xl mx-auto px-4">
+          <Card className="p-10 rounded-none mb-4">
+            <CardHeader>
+              <p>Pengajuan Peminjaman Mobil</p>
+            </CardHeader>
+            <CardContent>
+              <Table>
+                <TableCaption>A list of your recent invoices.</TableCaption>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead className="w-[100px]">Invoice</TableHead>
+                    <TableHead>Status</TableHead>
+                    <TableHead>Method</TableHead>
+                    <TableHead>Aksi</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {invoices.map((invoice) => (
+                    <TableRow key={invoice.invoice}>
+                      <TableCell className="font-medium">{invoice.invoice}</TableCell>
+                      <TableCell>{invoice.paymentStatus}</TableCell>
+                      <TableCell>{invoice.paymentMethod}</TableCell>
+                      <TableCell>
+                        <div className="flex justify-between">
+                          <Button>Tolak</Button>
+                          <Button>Tolak</Button>
+                        </div>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+                <TableFooter>
+                  <TableRow>
+                    <TableCell colSpan={3}>Total</TableCell>
+                    <TableCell className="text-right">$2,500.00</TableCell>
+                  </TableRow>
+                </TableFooter>
+              </Table>
+            
+            </CardContent>
+          </Card>
+        </div>
       </div>
       </>
   );
