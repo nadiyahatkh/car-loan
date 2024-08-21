f62eeac1308570611ec3b689c8c2aeb1b2906c4d
nadiyahatkh <nadiyahatikah843@gmail.com>
2024-08-21 14:08:02 +0700

    api,applicantuser

---
+++ src/app/apiService.jsx
@@ -0,0 +1,43 @@
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+export const fetchCar = async ({ token }) => {
+    try {
+      const response = await fetch(`${BASE_URL}/api/car`, {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+        }
+      });
+  
+      const data = await response.json();
+      return {
+        data,
+        message: "success"
+      };
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  };
+
+  export const fetchApplicantUser = async ({token}) => {
+    try {
+    //   const statusParams = status.map(s => `status[]=${s}`).join('&');
+        const response = await fetch(`${BASE_URL}/api/Applicant`, {
+          method: 'GET',
+          headers: {
+            'Authorization': `Bearer ${token}`,
+          }
+        })
+        .then((res) => res.json())
+        .then((data) => {
+         return {
+          data: data,
+          message: "successs"
+         }
+        })
+        return response.data
+      } catch (error) {
+        console.error(error);
+        return "abs";
+      }
+    };
--- src/app/user/submission-user/page.jsx
+++ src/app/user/submission-user/page.jsx
@@ -7,69 +7,80 @@ import { Table, TableBody, TableCaption, TableCell, TableFooter, TableHead, Tabl
 import { cn } from "@/lib/utils";
 import { CalendarIcon } from "@radix-ui/react-icons";
 import { addDays, format } from "date-fns";
-import { FolderXIcon } from "lucide-react";
+import { id } from "date-fns/locale";
+import { CheckCheckIcon, FolderXIcon, RefreshCcwIcon, XCircle } from "lucide-react";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from "@/components/ui/alert-dialog";
 import { TriangleAlert } from "lucide-react";
 import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
+import { fetchApplicantUser, fetchCar } from "@/app/apiService";
+import { useSession } from "next-auth/react";
 
 export default function SubmissionUser(){
+    const { data: session } = useSession();
+    const token = session?.user?.token;
+    const [data, setData] = useState([])
+    const[cars, setCars] = useState([])
   const [date, setDate] = useState({
       from: new Date(2022, 0, 20),
       to: addDays(new Date(2022, 0, 20), 20),
   });
 
-  const invoices = [
-    {
-      name: "INV001",
-      paymentStatus: "Paid",
-      paymentMethod: "Credit Card",
-    },
-    {
-      name: "INV002",
-      paymentStatus: "Pending",
-      paymentMethod: "PayPal",
-    },
-    {
-      name: "INV003",
-      paymentStatus: "Unpaid",
-      paymentMethod: "Bank Transfer",
-    },
-  ]
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        const response = await fetchCar({ token });
+        setCars(response.data.data);
+      } catch (error) {
+        console.error('Failed to fetch data:', error);
+      }
+    };
+    if (token) {
+      loadData();
+    }
+    }, [token]);
+
+    useEffect(() => {
+        const submissionData = async () => {
+          try {
+            const applicantData = await fetchApplicantUser({ token });
+            console.log(applicantData); 
+            setData(applicantData.Applicant.data);
+          } catch (error) {
+            console.error('Failed to fetch data:', error);
+          }
+        };
+      
+          if (token) {
+            submissionData();
+          }
+        }, [token]);
 
   return(
       <div className="w-full max-w-7xl mx-auto">
           <div className="flex flex-col md:flex-row items-center space-x-0 md:space-x-3 mb-5">
-            <Card className="rounded-none flex relative w-full md:w-auto">
+          {Array.isArray(cars) && cars.length > 0 ? (
+          cars.map((car) => (
+            <Card key={car.id} className="rounded-none flex relative w-full md:w-auto">
               <div className="absolute top-2 left-2 bg-gray-200 p-2 rounded-sm">
-                <p className="text-sm">Available Now</p>
+                <p className="text-sm">{car.status_name}</p>
               </div>
               <div className="flex flex-col p-4 pt-12">
-                <p className="font-bold text-sm">Avanza Veloz 2022</p>
+                <p className="font-bold text-sm">{car.name}</p>
               </div>
               <img 
-                src="/veloc.png" 
-                alt="veloc" 
+                src={car.path}
+                alt={car.name} 
                 className="h-24 w-55 ml-auto mt-4" 
               />
             </Card>
-
-            <Card className="rounded-none flex relative w-full md:w-auto mt-3 md:mt-0">
-              <div className="absolute top-2 left-2 bg-gray-200 p-2 rounded-sm">
-                <p className="text-sm">Available Now</p>
-              </div>
-              <div className="flex flex-col p-4 pt-12">
-                <p className="font-bold text-sm">Mobilio 2018</p>
-              </div>
-              <img 
-                src="/mobilio.png" 
-                alt="mobilio" 
-                className="h-24 w-55 ml-auto mt-4" 
-              />
-            </Card>
+          ))
+        ) : (
+          <p>No cars available.</p>  // Fallback if cars array is empty
+        )}
           </div>
 
 
@@ -140,15 +151,54 @@ export default function SubmissionUser(){
                               </TableRow>
                           </TableHeader>
                           <TableBody>
-                              {invoices.map((invoice) => (
-                                  <TableRow key={invoice.name}>
-                                      <TableCell className="text-sm">{invoice.name}</TableCell>
-                                      <TableCell className="text-sm">{invoice.paymentStatus}</TableCell>
-                                      <TableCell className="text-sm">{invoice.paymentStatus}</TableCell>
-                                      <TableCell className="text-sm">{invoice.paymentStatus}</TableCell>
-                                      <TableCell className="text-right flex">-</TableCell>
-                                  </TableRow>
-                              ))}
+                          {Array.isArray(data) && data.length > 0 ? (
+                                data.map((applicant) => {
+                                    let statusIcon;
+                                    let statusTextClass;
+
+                                    switch (applicant.status) {
+                                        case "Disetujui":
+                                            statusIcon = <CheckCheckIcon className="h-4 w-4 text-green-500" />;
+                                            statusTextClass = "text-green-500";
+                                            break;
+                                        case "Ditolak":
+                                            statusIcon = <XCircle className="h-4 w-4 text-red-500" />;
+                                            statusTextClass = "text-red-500";
+                                            break;
+                                        case "Belum Disetujui":
+                                            statusIcon = <RefreshCcwIcon className="h-4 w-4 text-black" />;
+                                            statusTextClass = "text-black";
+                                            break;
+                                        default:
+                                            statusIcon = null;
+                                            statusTextClass = "";
+                                            break;
+                                    }
+
+                                    return (
+                                        <TableRow key={applicant.id}>
+                                            <TableCell className="text-sm">{applicant.purpose}</TableCell>
+                                            <TableCell className="text-sm">
+                                                {applicant.submission_date ? format(new Date(applicant.submission_date), "dd MMMM yyyy, HH:mm 'WIB'", { locale: id }) : '-'}
+                                            </TableCell>
+                                            <TableCell className="text-sm">
+                                                {applicant.expiry_date ? format(new Date(applicant.expiry_date), "dd MMMM yyyy, HH:mm 'WIB'", { locale: id }) : '-'}
+                                            </TableCell>
+                                            <TableCell className={`text-sm flex items-center ${statusTextClass}`}>
+                                                {statusIcon}{applicant.status}
+                                            </TableCell>
+                                            <TableCell className="text-sm">{applicant.notes || '-'}</TableCell>
+                                        </TableRow>
+                                    );
+                                })
+                            ) : (
+                                <TableRow>
+                                    <TableCell colSpan={5} className="text-center text-sm">
+                                        No submissions available.
+                                    </TableCell>
+                                </TableRow>
+                            )}
+
                           </TableBody>
                       </Table>
                   </CardContent>
